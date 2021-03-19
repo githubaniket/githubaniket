f2afa2070738d1d14d2e9b71f56bffc38113b800
Aniket Verma <80255576+githubaniket@users.noreply.github.com>
2021-03-19 22:47:08 +0530

    qr-code-generator
    This is a QR-code generator using this you can make your own name QR-code or even make your youtube or any another link's QR-code.

---
+++ qrcodegenerator.py
@@ -0,0 +1,5 @@
+import qrcode #install qrcode modeule using this command "pip install qrcode"
+
+qr = qrcode.make('Hello guys') #make function is in qrcode module
+
+qr.save('qr.png') #save function also is in qrcode module
